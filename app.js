44e86501f5dbaaa837b1c277cde4e456e91ca05b
ZoePlayoust <playoustz@gmail.com>
2022-06-08 19:20:42 +0200

    Create the app

---
+++ app.js
@@ -0,0 +1,38 @@
+userName = "Zoé";
+userQuestion = "Will I get rich?";
+randomNumber = Math.floor(Math.random() * 8);
+eightBall = "";
+
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+userName
+  ? console.log(`Hello, ${userName}! you asked "${userQuestion}".`)
+  : console.log(`Hello, you asked "${userQuestion}".`);
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    ceightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "Outlook not so good";
+    break;
+  case 6:
+    eightBall = "My sources say no";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+
+console.log(`${eightBall}, my dear.`);
